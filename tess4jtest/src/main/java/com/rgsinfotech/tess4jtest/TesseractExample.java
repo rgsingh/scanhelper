056b9df6c16728fc9be376ab80c046af2b1150fe
unknown <rsingh@RGSMOBILE.accountvision.local>
2012-06-03 13:00:00 -0400

    Used absolute path for file name when using JNA Interface Mapping.

--- tess4jtest/src/main/java/com/rgsinfotech/tess4jtest/TesseractExample.java
+++ tess4jtest/src/main/java/com/rgsinfotech/tess4jtest/TesseractExample.java
@@ -1,17 +1,21 @@
 package com.rgsinfotech.tess4jtest;
 
 import java.io.File;
-import net.sourceforge.tess4j.*;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 
 public class TesseractExample {
 
     public static void main(String[] args) {
-        File imageFile = new File("/home/rsingh/workspace/tess4jtest/src/main/resources/eurotext.tif");
+    	
+    	File imageFile = new File("C:\\eclipse_workspaces\\development\\scanhelper\\tess4jtest\\src\\main\\resources\\eurotext.tif");
         Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
-//        Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+        //Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
 
         try {
             String result = instance.doOCR(imageFile);
+        	//String result = instance.doOCR(new File("bullpoop.tif"));
             System.out.println(result);
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
